af1fc19592e669ed12b88ac870389b3796bd26ff
Fay Koh <fay.koh@hotmail.com>
2016-08-15 18:49:12 -0400

    home, login, and signup pages basically done

---
+++ browser/js/signup/signup.js
@@ -0,0 +1,25 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: 'js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+    $scope.signup = {};
+    $scope.error = null;
+
+    $scope.sendSignup = function (signupInfo) {
+
+        $scope.error = null;
+
+        AuthService.signup(signupInfo).then(function () {
+            $state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid signup credentials.';
+        });
+    };
+});
