93af7925b23b908ac5c3c7226bd466682858a3a6
Fay Koh <fay.koh@hotmail.com>
2016-08-17 17:27:18 -0400

    ugly text box now in place

--- browser/js/entry/entry.js
+++ browser/js/entry/entry.js
@@ -2,21 +2,23 @@ app.config(function ($stateProvider) {
     $stateProvider.state('entry', {
         url: '/entry',
         templateUrl: 'js/entry/entry.html',
-        controller: 'EntryCtrl'
+        controller: 'TinyMceController'
     });
 });
 
-tinymce.init({
-  selector: 'textarea',
-  height: 500,
-  plugins: [
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table contextmenu paste code'
-  ],
-  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  content_css: [
-    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-    '//www.tinymce.com/css/codepen.min.css'
-  ]
+app.controller('TinyMceController', function($scope) {
+  $scope.tinymceModel = 'Initial content';
+
+  $scope.getContent = function() {
+    console.log('Editor content:', $scope.tinymceModel);
+  };
+
+  $scope.setContent = function() {
+    $scope.tinymceModel = 'Time: ' + (new Date());
+  };
+
+  $scope.tinymceOptions = {
+    plugins: 'link image code',
+    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+  };
 });
