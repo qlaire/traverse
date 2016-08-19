1a9f4de774349e4a21d2b298e034f4f2bb46e1cd
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-08-19 17:30:44 -0400

    sphere doesnt yet move with camera

--- world/sky.js
+++ world/sky.js
@@ -1,8 +1,9 @@
 function addSky(){
 	var geometry = new THREE.SphereGeometry(2000, 60, 40);  
 
+
 	var material = new THREE.MeshLambertMaterial( {  
-	  color: 'white'
+	  color: 'white', map: THREE.ImageUtils.loadTexture('assets/cloud.png')
 	});
 
 	skyBox = new THREE.Mesh(geometry, material);  
@@ -10,4 +11,6 @@ function addSky(){
 	skyBox.eulerOrder = 'XZY';  
 	skyBox.renderDepth = 1000.0;  
 	scene.add(skyBox);  
+	scene.fog = new THREE.FogExp2( 'white', 0.00025 );
+
 }
