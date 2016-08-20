52e06613d9dcf98d8f29fd81feb576cd7535961f
Emily Gok Lucas <eclucas@calpoly.edu>
2016-08-20 15:27:57 -0400

    added single entry

---
+++ browser/js/singleEntry/singleEntry.js
@@ -0,0 +1,43 @@
+app.config(function($stateProvider) {
+    $stateProvider.state('entry', {
+        params: {
+            entry: null
+        },
+        url: '/singleEntry',
+        templateUrl: 'js/singleEntry/singleEntry.html',
+        controller: 'EntryController',
+        data: {
+            bodyClass: 'bg4'
+        }
+    });
+});
+
+app.controller('EntryController', function($scope) {
+    $scope.tinymceModel = 'How are you feeling today?';
+
+    $scope.getContent = function() {
+        console.log('Editor content:', $scope.tinymceModel);
+    };
+
+    $scope.setContent = function() {
+        $scope.tinymceModel = 'Time: ' + (new Date());
+    };
+
+    $scope.tinymceOptions = {
+        selector: 'div.tinymce',
+        theme: 'inlite',
+        plugins: 'autoresize',
+        autoresize_max_height:500,
+        selection_toolbar: 'bold italic underline strikethrough | blockquote',
+        inline: true,
+        paste_data_images: false
+    };
+});
+
+app.factory('EntryFactory', function ($http) {
+  let entryObj = {};
+  entryObj.analyzeEntry = function (entry) {
+
+  }
+  return entryObj;
+});
