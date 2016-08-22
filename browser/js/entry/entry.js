cc6dcada024aca771608498e3a0a1596e0c83bcd
Claire Dranginis <cdranginis@gmail.com>
2016-08-22 13:57:40 -0400

    renamed subject to title and entry posts to database

--- browser/js/entry/entry.js
+++ browser/js/entry/entry.js
@@ -6,7 +6,7 @@ app.config(function($stateProvider) {
     });
 });
 
-app.controller('EntryController', function($scope, $log) {
+app.controller('EntryController', function($scope, $log, EntryFactory, $state) {
   $scope.tinymceModel = '';
   $scope.errorMessage = {};
 
@@ -24,39 +24,40 @@ app.controller('EntryController', function($scope, $log) {
         if($scope.tinymceModel.match(reg)){
             $scope.errorMessage.reg = '';
         } else {
-            $scope.errorMessage.reg = 'Your post must contain at least a one word sentence. Sentences have punctuation!';
+            $scope.errorMessage.reg = 'Your post must contain at least a one sentence. Sentences have punctuation!';
         }
     }
     else{
         $scope.errorMessage.length = 'Your post is too short.';
-        $scope.errorMessage.reg = 'Your post must contain at least a one word sentence. Sentences have punctuation!';
+        $scope.errorMessage.reg = 'Your post must contain at least a one sentence. Sentences have punctuation!';
     }
     return $scope.tinymceModel.length > 17 && $scope.tinymceModel.match(reg)
   }
 
-  $scope.getContent = function() {
-    console.log('Editor content:', $scope.tinymceModel);
+  $scope.postEntry = function () {
+    EntryFactory.postEntry($scope.tinymceModel, $scope.title)
+    .then(data => {
+      $state.go('entries');
+    })
+    .catch($log.error);
+  }
+  
+  $scope.tinymceOptions = {
+      selector: 'div.tinymce',
+      theme: 'inlite',
+      plugins: 'autoresize',
+      autoresize_max_height:500,
+      selection_toolbar: 'bold italic underline strikethrough | blockquote',
+      inline: true,
+      paste_data_images: false
   };
-
-  $scope.setContent = function() {
-    $scope.tinymceModel = 'Time: ' + (new Date());
-  };
-
-    $scope.tinymceOptions = {
-        selector: 'div.tinymce',
-        theme: 'inlite',
-        plugins: 'autoresize',
-        autoresize_max_height:500,
-        selection_toolbar: 'bold italic underline strikethrough | blockquote',
-        inline: true,
-        paste_data_images: false
-    };
 });
 
 app.factory('EntryFactory', function ($http) {
   let entryObj = {};
-  entryObj.analyzeEntry = function (entry) {
-
+  entryObj.postEntry = function (body, title) {
+    return $http.post('/api/entries/', {entry: body, title: title})
+    .then(res => res.data);
   }
   return entryObj;
 });
--- seed.js
+++ seed.js
@@ -16,7 +16,7 @@ var Promise = require('sequelize').Promise;
 var createdEntries;
 var seedEntries = function() {
   var entries = [{
-    subject: "bananas",
+    title: "bananas",
     body: "I love bananas a lot.",
     date: "2016-01-18 07:36:21-04",
     joy: [0.8, 0.2, 0.3],
@@ -24,7 +24,7 @@ var seedEntries = function() {
     fear: [0.3, 0.2, 0.1],
     keywords: [{ 'bananas': '0.1' },{'like':'.6'},{'lot':'.7'},{'white house':.6
   }, {'first lady':.7},{'underwater nachos':.4},{'grandmother':.7},{'weasle boyfriend':.6},{'cherries':.1},{'racoon dance':.5}],
-    subject: "cheese",
+    title: "cheese",
     body: "I love cheese a bunch.",
     date: "2016-01-31 10:45:20-04",
     joy: [0.8, 0.2, 0.3],
@@ -32,7 +32,7 @@ var seedEntries = function() {
     fear: [0.3, 0.2, 0.1],
     keywords: [{"meal":"0.9","cheese":"0.9","devour":"0.8","hunger":"0.7","price":"0.7"},{"unhappy":"1.0","strength":"0.9","doctor":"0.9","protein":"0.8","visitors":"0.8"},{"spatula":"0.9","red apron":"0.8","prom":"0.7","tigers":"0.7"}]
   }, {
-    subject: "apples",
+    title: "apples",
     body: "I love apples.",
     date: "2016-03-15 19:30:05-04",
     joy: [0.8, 0.2, 0.3],
@@ -40,7 +40,7 @@ var seedEntries = function() {
     fear: [0.3, 0.2, 0.1],
     keywords: [{"school":"0.9","boys":"0.9","cafeteria":"0.8","homework":"0.7","best friends":"0.7"},{"gratitude":"1.0","growth":"0.9","leave behind":"0.9","job":"0.8","summer vacation":"0.8"},{"pool":"0.9","swimming":"0.8","prom":"0.7","tigers":"0.7"}]
   }, {
-    subject: "sleepy",
+    title: "sleepy",
     body: "I am very tired",
     date: "2016-04-02 12:15:21-04",
     joy: [0.1, 0.0, 0.0],
@@ -48,7 +48,7 @@ var seedEntries = function() {
     fear: [0.3, 0.2, 0.1],
     keywords: [{"forests":"0.9","wandering":"0.9","darkness":"0.8","trees":"0.7","shadpws":"0.7"},{"windows":"1.0","deep water":"0.9","injustice":"0.9","existential":"0.8","torrent":"0.8"},{"silver fish":"0.9","rangers":"0.8","mountain cabin":"0.7","freedom":"0.7"}]
   }, {
-    subject: "I'm sad",
+    title: "I'm sad",
     body: "I've been depressed since december last year, possibly longer. Before december I had about a month period where I actually felt happy for a change. Before that I just wanted to drink my sorrows away every day and the loneliness was killing me. Now, in january a relationship I was in ended. I've been depressed pretty much ever since, but my depression keeps changing in it's symptoms. For 6 months since January untill I got over her I was severely depressed due to losing a friend among other things as well and I was suicidal, self-harming at some point, etc. When I got over her, I was still depressed, somewhat midly. But then it gets worse. I've read about different kinds of depression and one week I relate completely to one type, while the next week the other. The symptoms change. Now I'm restless and fidgety. I wasn't like that last week. So, it's difficult to decide what to answer to online depression tests. Anyone else with a similar experience?",
     date: "2016-05-01 04:36:21-04",
     joy: [0.1,0,0],
@@ -56,7 +56,7 @@ var seedEntries = function() {
     fear: [0.2,0.2,0.3],
     keywords: [{"loneliness":"0.9","sorrows":"0.9","period":"0.8","change":"0.7","relationship":"0.7"},{"depression":"1.0","symptoms":"0.9","friend":"0.9","things":"0.8","point":"0.8"},{"different kinds":"0.9","depression tests":"0.8","symptoms change":"0.7","similar experience":"0.7"}]
   }, {
-    subject: "I'm an eagle scout",
+    title: "I'm an eagle scout",
     body: "Yesterday, I officially obtained the rank of eagle scout. It is Boy Scouting's highest rank. Only about 5% of boys who join scouting obtain the rank of eagle! I have been working towards this for that past 8 years. After a lot of hard work it feels great to have achieved such an honor. The one thing that I have stuck with for most of my life(I started as a cub scout) has become such a foundation for who I am. The process for the rank is no easy task. I had to earn several merit badges. I also had to plan and lead a service project. For my project I build a dog walking trail for a local animal shelter to walk the shelter dogs on. I just wanted to share this because it is something that finally getting has made me so happy. Also if you are a boy scout and are working towards your eagle scout, keep working for it. It is a wonderful honor. Nothing in my life has made me more happy than being told that I was officially and eagle scout.",
     date: "2016-08-18 13:36:21-04",
     joy: [0.2,0.4,0.8],
