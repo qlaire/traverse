4dd29e44eeda324111e3888cb06110dda348bf84
Claire Dranginis <cdranginis@gmail.com>
2016-08-22 17:01:28 -0400

    starting to implement glow

--- world/columns.js
+++ world/columns.js
@@ -1,6 +1,6 @@
 function createColumn(x, y, z) {
   var geometry = new THREE.CylinderGeometry( 5, 5, 4000, 32 );
-  var material = new THREE.MeshBasicMaterial( {color: 0x7bbdec, wireframe: true} );
+  var material = new THREE.MeshBasicMaterial( {color: 0x7bbdec} );
   var cylinder = new THREE.Mesh( geometry, material );
   cylinder.position.x = x;
   cylinder.position.y = y;
