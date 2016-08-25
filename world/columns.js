5dfb86c4b45aaf2875c6b10d5b37f46b2e246c91
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-08-25 12:00:10 -0400

    fixed

--- world/columns.js
+++ world/columns.js
@@ -86,17 +86,17 @@ function columnUpdate(ts)
 }
 
 function placeColumns() {
-  let numColumns = Math.ceil(Object.keys(xZones).length / 60);
-  numColumns=1;
+  // let numColumns = Math.ceil(Object.keys(xZones).length / 60);
+  // numColumns=1;
   let zCoord, xCoord;
   let yCoord = 0;
   var cylinder;
-  for (var i = 0; i < numColumns; i++) {
-    zCoord = zZones[999]-Math.random()*(zZones[999]-zZones[2]);
-    var bound=Math.floor((Object.keys(xZones).length-2)/2)
-    xCoord = xZones[0] + Math.random() * (xZones[bound] - xZones[0]);
-    cylinder=createColumn(xCoord, yCoord, zCoord);
-  }
+  // for (var i = 0; i < numColumns; i++) {
+  zCoord = zZones[999]-Math.random()*(zZones[999]-zZones[2]);
+  var bound=Math.floor((Object.keys(xZones).length-2)/2)
+  xCoord = xZones[0] + Math.random() * (xZones[bound] - xZones[0]);
+  cylinder=createColumn(xCoord, yCoord, zCoord);
+  // }
   return cylinder.position;
 
 }
